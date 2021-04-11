dc42bf637fc5211aab8e6e82cb4096cd2e9f947e
Victor Godoka <martinsvictorsantos@gmail.com>
2021-04-11 16:58:06 -0300

    RPG-000: create react app ts

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './pages/Home';
+
+function App () {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
