af563e127ae732157ed2271fb864997f5d51b5b5
Victor Godoka <martinsvictorsantos@gmail.com>
2021-04-13 14:59:50 -0300

    RPG-000: created basic mana color css

---
+++ src/components/manaStyles.tsx
@@ -0,0 +1,28 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+const manaStyles = makeStyles({
+  mana: {
+    color: '#fff',
+    borderRadius: '50%',
+    fontSize: '.9rem',
+    padding: '5px'
+  },
+  r: {
+    backgroundColor: '#7e0017'
+  },
+  u: {
+    backgroundColor: '#244492'
+  },
+  b: {
+    backgroundColor: '#430163'
+  },
+  w: {
+    backgroundColor: '#e59d10'
+  },
+  g: {
+    backgroundColor: '#006732'
+  },
+})
+
+
+export default manaStyles
