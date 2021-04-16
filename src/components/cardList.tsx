0eb37ed725f4fe1037da96a71f1b50b826fcd660
Victor Martins and Larissa Moura <victor.martins+larissa.moura@thoughtworks.com>
2021-04-16 09:52:17 -0300

    adding stuff

--- src/components/cardList.tsx
+++ src/components/cardList.tsx
@@ -1,25 +1,61 @@
 import React from 'react'
-import styled from 'styled-components'
+import Close from "@material-ui/icons/Close";
+import { makeStyles } from "@material-ui/core/styles";
+import SpellCard from './spellCard'
+import { green } from "@material-ui/core/colors";
 
-const Wrapper = styled.ul`
-  display: grid;
-  list-style: none;
-  margin: 0 auto;
-  grid-template-columns: repeat(${() => Math.floor(window.innerWidth / 100)}, 1fr);
-  grid-gap: 4px;
-`
+const useStyles = makeStyles({
+  wrapper: {
+    display: "flex",
+    listStyle: "none",
+    margin: "0 auto",
+    flexWrap: "wrap",
+    position: `fixed`,
+    top: "20vh",
+    left: "10vw",
+    width: "80vw",
+    height: "60vh",
+    background: "rgba(255,255,255, .5)",
+    padding: "20px",
+    alignItems: "start",
+    justifyContent: "start",
+    overflowX: "auto",
+    overflowY: "hidden",
+    borderRadius: "10px",
+    zIndex: 4,
+    border: "2px solid rgba(0,0,0, .25)",
+  },
+  closeButton: {
+    position: `absolute`,
+    right: "0",
+    top: "0",
+    borderRadius: "50px",
+    width: `50px`,
+    height: `50px`,
+    background: "rgba(255,255,255,1)",
+    zIndex: 8,
+    cursor: "pointer",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    "& svg": {
+      fontSize: `2rem`,
+      color: `#000`,
+    },
+  },
+});
 
-const Line = styled.li`
-  cursor: hover;
-`
+export default function CardList({ cards, close }) {
+  const classes = useStyles()
 
-const Image = styled.img`
-  width: 100%;
-`
-
-export default function CardList({ cards, selectCard }) {
-
-  return <Wrapper>
-    {cards.map((card, i) => <Line key={i} onClick={() => selectCard(card)}><Image title={card?.name} src={card?.image_uris?.png} /></Line>)}
-  </Wrapper>
+  return (
+    <div className={classes.wrapper}>
+      <div onClick={close} className={classes.closeButton}>
+        <Close />
+      </div>
+      {cards.map((spell, i) => (
+        <SpellCard spell={spell} />
+      ))}
+    </div>
+  );
 }
--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -4,11 +4,49 @@ import { TextField } from "@material-ui/core";
 import SpellLibrary from '../db/SpellLibrary.json'
 import AutoComplete from "../components/cardAutocomplete";
 import SpellList from "../components/spellList";
+import List from '@material-ui/icons/List';
+import { makeStyles } from '@material-ui/core/styles'
+import CardList from "../components/cardList";
+
+const useStyles = makeStyles({
+  library__wrapper: {
+    width: '100%',
+    padding: '20px',
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  library__base: {
+    backgroundColor: '#ffffff20',
+    borderRadius: '4px 0',
+    position: 'relative',
+    width: '100%',
+    "& .MuiFormLabel-root": {
+      color: '#919191 !important'
+    },
+    "& .MuiInputBase-input": {
+      color: '#fff !important'
+    },
+    "& .MuiFilledInput-underline:after": {
+      borderBottom: '2px solid #000'
+    }
+  },
+  library__icon: {
+    position: 'absolute',
+    right: '10px',
+    top: '12px',
+    color: '#fff',
+    fontSize: '2rem',
+    zIndex: 2,
+    cursor: 'pointer'
+  }
+})
 
 const Library: React.FC = () => {
+  const classes = useStyles()
   const [cards, setCards] = useState<any[]>([]);
   const [spellList, setSpellList] = useState<any[]>([]);
   const [value, setValue] = useState<string>("");
+  const [showSpellList, setShowSpellList] = useState<boolean>(false);
 
   const debouncedSave = useRef(debounce((nextValue: string) => findACard(nextValue), 500)).current;
 
@@ -32,10 +70,9 @@ const Library: React.FC = () => {
   }
 
   return (
-    <div>
-      <div>
+    <div className={classes.library__wrapper}>
+      <div className={classes.library__base}>
         <TextField
-          style={{ position: 'relative' }}
           id="filled-basic"
           label="Select a Spell"
           variant="filled"
@@ -44,10 +81,19 @@ const Library: React.FC = () => {
           fullWidth
           onChange={handleChange}
         />
+        <List
+          className={classes.library__icon}
+          onClick={() => setShowSpellList(!showSpellList)}
+        />
       </div>
-      <AutoComplete spellList={spellList} selectCard={selectCardAndSave} cards={cards} />
+      <AutoComplete
+        spellList={spellList}
+        selectCard={selectCardAndSave}
+        cards={cards}
+      />
 
       <SpellList spellList={spellList} />
+      {showSpellList && <CardList close={() => setShowSpellList(false)} cards={SpellLibrary} />}
     </div>
   );
 };
